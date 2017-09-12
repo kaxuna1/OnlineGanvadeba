ca8f439dd7b71d19de64e5f986dfada8329cc51f
kaxuna1 <kaxgel11@gmail.com>
2017-09-12 11:11:42 +0400

    commit

---
+++ Backup1/OnlineGanvadeba/Controllers/AppController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace OnlineGanvadeba.Controllers
+{
+    public class AppController : ApiController
+    {
+
+
+        [HttpGet]
+        [Route("api/app/t")]
+        public string t()
+        {
+            return "kaxa";
+        }
+    }
+}
